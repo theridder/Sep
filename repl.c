48e07c1ca40513a7425abc7adc743f738878d814
Rick Ridder <rick.ridder@yahoo.es>
2018-08-29 16:13:18 +0100

    Started work on parser, repl now transforms input into AST

--- repl.c
+++ repl.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 /* On Windows editline functionality is redundant, we replace it with this */
 #ifdef _WIN32
@@ -29,11 +30,27 @@ void add_history(char* arg) {}
 
 int main(int argc, char** argv) {
 
+    /* Create some parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Sep = mpc_new("sep");
+
+    /* Define them */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                       \
+          number    : /-?[0-9]+/ ;                              \
+          operator  : '+' | '-' | '*' | '/' ;                   \
+          expr      : <number> | '(' <operator> <expr>+ ')' ;   \
+          sep       : /^/ <operator> <expr>+ /$/ ;              \
+        ",
+        Number, Operator, Expr, Sep);
+
     /* Print Version and other info */
     puts("Interactive Sep Version 0.01");
     puts("Press Ctrl+c to Exit\n");
 
-    /* Loops until program is killed */
+    /* Loops until ctrl-c */
     while (1) {
 
         /* Print prompt and get input */
@@ -42,12 +59,24 @@ int main(int argc, char** argv) {
         /* Add input to history, to be retrieved with arrow keys */
         add_history(input);
 
-        /* Echo input */
-        puts(input);
+        /* Attempt to parse input */
+        mpc_result_t result;
+        if (mpc_parse("<stdin>", input, Sep, &result)) {
+            /* On success print the AST */
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        } else {
+            /* Otherwise Print the Error */
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
 
         free(input);
 
     }
+
+    /* Undefine and delete parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Sep);
 
     return 0;
 }
