358d0b68c074bbd984d7b7300a52b4b430b44075
Rick Ridder <rick.ridder@yahoo.es>
2018-09-01 14:55:35 +0100

    Basic mathematical expressions are now evaluated. Basic error handling is supported (division by zero, most notably).

--- repl.c
+++ repl.c
@@ -28,6 +28,115 @@ void add_history(char* arg) {}
 #include <editline/history.h>
 #endif
 
+
+/*Create enumeration of possible error types */
+enum { SERR_DIV_ZERO, SERR_BAD_OP, SERR_BAD_NUM };
+
+/* Create enumeration of possible sval types */
+enum { SVAL_NUM, SVAL_ERR };
+
+/* Declare sval struct */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} sval;
+
+/* Number type sval constructor */
+sval sval_num(long x) {
+    sval new;
+    new.type = SVAL_NUM;
+    new.num = x;
+    return new;
+}
+
+/* Error type sval constructor */
+sval sval_err(int x) {
+    sval new;
+    new.type = SVAL_ERR;
+    new.err = x;
+    return new;
+}
+
+/* Print an sval value */
+void sval_print(sval value) {
+    switch (value.type) {
+        /* If value is a number, print it */
+        case SVAL_NUM:
+            printf("%li", value.num);
+            break;
+
+        /* If value is an error, output appropiate message */
+        case SVAL_ERR:
+            switch (value.err) {
+                case SERR_DIV_ZERO:
+                    printf("Error: Division by zero");
+                    break;
+                case SERR_BAD_OP:
+                    printf("Error: Invalid operator");
+                    break;
+                case SERR_BAD_NUM:
+                    printf("Error: Invalid number");
+                    break;
+                default:
+                    printf("Error: Unknown error");
+            } break;
+
+        /* Impossible case */
+        default:
+            printf("Critical error: Invalid sval type");
+    }
+}
+
+/* Convenience function, adds a newline to sval_print */
+void sval_println(sval value) { sval_print(value); putchar('\n'); }
+
+
+/* Use operator string to see which operation to perform */
+sval eval_op(sval x, char* op, sval y) {
+    /* If either value is an error, return it */
+    if (x.type == SVAL_ERR) { return x; }
+    if (y.type == SVAL_ERR) { return y; }
+
+    /* Otherwise, evaluate */
+    if (strcmp(op, "+") == 0) { return sval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return sval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return sval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+        /* If second operand is zero return error */
+        return y.num == 0 ? sval_err(SERR_DIV_ZERO) : sval_num(x.num / y.num);
+    }
+
+    return sval_err(SERR_BAD_OP);
+}
+
+/* Evaluate an expression */
+sval eval(mpc_ast_t* tree) {
+
+    /* If node is tagged as number, return it */
+    if (strstr(tree->tag, "number")) {
+        /* Check if there is some error in conversion */
+        errno = 0;
+        long x = strtol(tree->contents, NULL, 10);
+        return errno != ERANGE ? sval_num(x) : sval_err(SERR_BAD_NUM);
+    }
+
+    /* Otherwise retrieve the operator */
+    char* op = tree->children[1]->contents;
+
+    /* We store the first argument in 'x' */
+    sval x = eval(tree->children[2]);
+
+    /* Iterate the remaning children, applying operator. */
+    int i = 3;
+    while (strstr(tree->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(tree->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     /* Create some parsers */
@@ -62,8 +171,9 @@ int main(int argc, char** argv) {
         /* Attempt to parse input */
         mpc_result_t result;
         if (mpc_parse("<stdin>", input, Sep, &result)) {
-            /* On success print the AST */
-            mpc_ast_print(result.output);
+            /* On succesfull parse, return the value of the computation */
+            sval output = eval(result.output);
+            sval_println(output);
             mpc_ast_delete(result.output);
         } else {
             /* Otherwise Print the Error */
